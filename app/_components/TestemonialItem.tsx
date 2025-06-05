76cda2d9544dd5754069afe0b337f0733390033a
RamtinRmzani <Ramtinramzani76@gmail.com>
2025-06-05 20:46:52 +0330

    fix for deploying (some error in testemonila and repayment) - comment repayment for now

--- app/_components/TestemonialItem.tsx
+++ app/_components/TestemonialItem.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import pic from "@/../public/images/jonior/testemonial-grid/image-daniel.jpg";
 
 interface TestemonialItemProps {
   title: string;
--- app/projects/jonior/repayment-calculator/page.tsx
+++ app/projects/jonior/repayment-calculator/page.tsx
@@ -1,26 +1,26 @@
-"use client";
+// "use client";
 
-import RepaymentForm from "@/_components/forms/RepaymentForm";
-import ShowResultRepayment from "@/_components/forms/ShowResultRepayment";
-import { useState } from "react";
+// import RepaymentForm from "@/_components/forms/RepaymentForm";
+// import ShowResultRepayment from "@/_components/forms/ShowResultRepayment";
+// import { useState } from "react";
 
 function Page() {
-  const [monthlyRepayment, setMonthlyRepayment] = useState(0);
-  const [totalRepayment, setTotalRepayment] = useState(0);
+  // const [monthlyRepayment, setMonthlyRepayment] = useState(0);
+  // const [totalRepayment, setTotalRepayment] = useState(0);
 
-  function handleCalculate(monthly: number, total: number) {
-    setMonthlyRepayment(monthly);
-    setTotalRepayment(total);
-  }
+  // function handleCalculate(monthly: number, total: number) {
+  //   setMonthlyRepayment(monthly);
+  //   setTotalRepayment(total);
+  // }
 
   return (
     <div className="sm:max-w-3xl max-sm:w-[clamp(300px,2vw,700px)] relative mx-auto mt-[clamp(10px,4vw,144px)] sm:border border-primary-700 rounded-md shadow-md flex max-sm:flex-col justify-between sm:gap-2 text-gray-50 mb-10">
-      <RepaymentForm onCalculate={handleCalculate} />
+      {/* <RepaymentForm onCalculate={handleCalculate} />
 
       <ShowResultRepayment
         monthlyRepayment={monthlyRepayment}
         totalRepayment={totalRepayment}
-      />
+      /> */}
     </div>
   );
 }
